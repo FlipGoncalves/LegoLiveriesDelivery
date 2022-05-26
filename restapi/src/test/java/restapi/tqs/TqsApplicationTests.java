bb6d656d7682013175c5b859c537a88a290824a2
FlipGoncalves <filipeg@ua.pt>
2022-05-26 16:53:30 +0100

    rest api with UI dashboard template

---
+++ restapi/src/test/java/restapi/tqs/TqsApplicationTests.java
@@ -0,0 +1,13 @@
+package restapi.tqs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TqsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
