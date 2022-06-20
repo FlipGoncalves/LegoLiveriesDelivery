51c21dfde793817e8908162592ae62e2e016e3c6
FlipGoncalves <filipeg@ua.pt>
2022-06-20 21:37:38 +0100

    done ?

--- engine/src/main/java/tqs/project/model/Rider.java
+++ engine/src/main/java/tqs/project/model/Rider.java
@@ -23,7 +23,7 @@ import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "riderId")
 public class Rider {
     
-    @Id
+    @Id 
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "rider_id")
     private long riderId;
